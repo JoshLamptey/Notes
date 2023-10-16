9acef5fc22b1170628e63e40a4fa228c09f9fd40
JoshLamptey <107364497+JoshLamptey@users.noreply.github.com>
2023-10-15 19:49:21 -1200

    foundation

--- special-notes/src/App.jsx
+++ special-notes/src/App.jsx
@@ -21,6 +21,57 @@ export default function App(){
     setCurrentNoteId(NewNote.id)
   }
 
-  
+  function updateNotes(text){
+    setNotes(oldNotes=>oldNotes.map(oldNote=>{
+      return oldNote.id === currentNoteId
+      ?{...oldNote,body:text}
+      : oldNote
+    }))
+  }
+
+  function findCurrentNotes(){
+    return   notes.find((note)=> {
+    note.id===currentNoteId
+  }) || notes[0]
+  }
+
+  return(
+    <main>
+      {
+     notes.length > 0
+     ?
+      <Split
+      sizes={[30,70]}
+      direction="horizontal"
+      className="split"
+      >
+     <Sidebar
+     notes={notes}
+     currentNote={findCurrentNotes()}
+     setCurrentNoteId ={setCurrentNoteId}
+     newNote={createNewNote}
+     />
+     {
+      currentNoteId && 
+      notes.length > 0 &&
+      <Editor
+      currentNote = {findCurrentNotes}
+      updateNotes = {updateNotes}
+      />
+     }
+   </Split>
+    :
+    <div className="no-notes">
+      <h1>You Have No Notes</h1>
+      <button 
+      className="first-note"
+      onClick={createNewNote}
+      >
+        Create One Now
+      </button>
+    </div>
+  }
+    </main>
+  )
 
 }
