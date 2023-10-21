7a3fefd09f09e8a8875f63975d359f6e685d6be7
JoshLamptey <107364497+JoshLamptey@users.noreply.github.com>
2023-10-21 05:20:55 -1200

    [improve]{this is the  focus of the developers day

--- special-notes/src/components/Editor.jsx
+++ special-notes/src/components/Editor.jsx
@@ -1,5 +1,28 @@
+import showdown from 'showdown';
+import ReactMde from 'react-mde';
+import 'react-mde/lib/styles/css/react-mde-all.css';
+import { useState } from 'react';
 
+export default function Editor({ currentNote, updateNote }) {
+  const [selectedTab, setSelectedTab] = useState('write');
 
-export default function Editor(props){
-    
+  const converter = new showdown.Converter({
+    tables: true,
+    simplifiedAutoLink: true,
+    strikethrough: true,
+    tasklists: true,
+  });
+  return (
+    <section className="pane editor">
+      <ReactMde
+        value={currentNote.body}
+        onChange={updateNote}
+        selectedTab={selectedTab}
+        onTabChange={setSelectedTab}
+        generateMarkdownPreview={(markdown) => Promise.resolve(converter.makeHtml(markdown))}
+        minEditorHeight={80}
+        heightUnits="vh"
+      />
+    </section>
+  );
 }
