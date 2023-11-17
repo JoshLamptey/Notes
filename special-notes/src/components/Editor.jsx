48a91360dc4dedda8fb371357e4ff0db58d66ffd
JoshLamptey <107364497+JoshLamptey@users.noreply.github.com>
2023-11-17 05:58:28 -1200

    second

--- special-notes/src/components/Editor.jsx
+++ special-notes/src/components/Editor.jsx
@@ -3,7 +3,7 @@ import ReactMde from 'react-mde';
 import 'react-mde/lib/styles/css/react-mde-all.css';
 import { useState } from 'react';
 
-export default function Editor({ currentNote, updateNotes }) {
+export default function Editor({ tempNoteText, setTempNoteText }) {
   const [selectedTab, setSelectedTab] = useState('write');
 
   const converter = new showdown.Converter({
@@ -15,8 +15,8 @@ export default function Editor({ currentNote, updateNotes }) {
   return (
     <section className="pane editor">
       <ReactMde
-        value={currentNote?.body}
-        onChange={updateNotes}
+        value={tempNoteText}
+        onChange={setTempNoteText}
         selectedTab={selectedTab}
         onTabChange={setSelectedTab}
         generateMarkdownPreview={(markdown) => Promise.resolve(converter.makeHtml(markdown))}
